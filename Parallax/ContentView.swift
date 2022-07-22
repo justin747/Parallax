e582ec8100745858eb409617fb7226caec8b75a9
justin747 <justin747@gmail.com>
2022-07-22 15:41:14 -0700

    Initial Commit

---
+++ Parallax/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Parallax
+//
+//  Created by Justin on 7/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Parallax/ParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxApp.swift
+//  Parallax
+//
+//  Created by Justin on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
